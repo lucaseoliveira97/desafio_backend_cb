1777e4e6a1642e22130396ea5ed9b8e9bb594953
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-29 16:28:18 -0300

    :recycle: refactor: Renomeando entidades

---
+++ src/main/java/github/com/lucaseoliveira/desafiobackcb/adapter/out/persistence/entity/BranchEntity.java
@@ -0,0 +1,70 @@
+package github.com.lucaseoliveira.desafiobackcb.adapter.out.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Table(name = "branches")
+public class BranchEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    String name;
+    String cnpj;
+    String city;
+    String uf;
+    String type;
+    boolean active;
+    LocalDateTime registrationDate;
+    LocalDateTime lastUpdate;
+    @OneToMany(mappedBy="branch")
+    private List<SellerEntity> sellers;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+}
---
+++ src/main/java/github/com/lucaseoliveira/desafiobackcb/adapter/out/persistence/entity/SellerEntity.java
@@ -0,0 +1,69 @@
+package github.com.lucaseoliveira.desafiobackcb.adapter.out.persistence.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Date;
+
+
+@Entity
+@Table(name = "sellers")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+public class SellerEntity {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    private Long id;
+    private String registration;
+    @Column(nullable = false)
+    private String name;
+    private LocalDate birthDate;
+    @Column(nullable = false)
+    private String cpfCnpj;
+    @Column(nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String hiringType;
+    @ManyToOne
+    @JoinColumn(name = "branch_id")
+    private BranchEntity branch;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getRegistration() {
+        return registration;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public String getCpfCnpj() {
+        return cpfCnpj;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getHiringType() {
+        return hiringType;
+    }
+    public BranchEntity getBranch() {
+        return branch;
+    }
+}
