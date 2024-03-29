044eb8ce8cf1a7f0dc16beb14be3f989cae91e9c
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-29 18:40:20 -0300

    :test_tube: test: Mockando testes de obter lista de vendedores

--- src/test/java/github/com/lucaseoliveira/desafiobackcb/adapter/in/rest/GetSellerApiTests.java
+++ src/test/java/github/com/lucaseoliveira/desafiobackcb/adapter/in/rest/GetSellerApiTests.java
@@ -1,22 +1,43 @@
 package github.com.lucaseoliveira.desafiobackcb.adapter.in.rest;
 
+import github.com.lucaseoliveira.desafiobackcb.adapter.in.rest.dto.GetSellerDto;
+import github.com.lucaseoliveira.desafiobackcb.application.core.domain.Branch;
+import github.com.lucaseoliveira.desafiobackcb.application.core.domain.Seller;
+import github.com.lucaseoliveira.desafiobackcb.application.core.usecase.in.GetSellersUseCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @WebFluxTest
 public class GetSellerApiTests {
     @Autowired
     WebTestClient client;
 
+    @MockBean
+    private GetSellersUseCase getSellersUseCase;
     @Test
     void getSellersSuccess()
     {
+        List<Seller> sellers = new ArrayList<Seller>();
+        sellers.add(new Seller(1L,"reg1", "name1", null,"123", "a@a.com",
+                "CLT",new Branch(0L, "branch 1", "123", "campinas","sp","CLT",
+                true, null, null)));
+        when(getSellersUseCase.getSellers()).thenReturn(sellers);
         client.get().uri("/sellers")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isOk();
+                .expectStatus().isOk()
+                .expectBody(GetSellerDto[].class)
+                .value(dto -> assertThat(dto.length).isEqualTo(1));
+
     }
 }
