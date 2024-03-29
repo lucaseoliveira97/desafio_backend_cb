5361c9fdd87322027d69444c9b648ce9f322f12d
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-29 12:23:09 -0300

    :bricks: ci: Mock de dados de filiais e criacao da base

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO BRANCH (name, cnpj, city, uf,type,active ,registration_date) VALUES
+    ('Branch1', '123123123123', 'Sao Paulo', 'SP', 'type1', TRUE, {ts '2021-09-17 18:47:52.69'}),
+    ('Branch2', '456456456456', 'Rio de Janeiro', 'RJ', 'type2', TRUE, {ts '2022-01-20 10:27:32.59'});
