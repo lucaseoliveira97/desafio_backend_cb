187eec9ecee615141569b0a910023de1236100c1
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-29 18:37:53 -0300

    :sparkles: feat: Criando classe de mapeamento de vendedores

---
+++ src/main/java/github/com/lucaseoliveira/desafiobackcb/adapter/mapper/SellerMapper.java
@@ -0,0 +1,19 @@
+package github.com.lucaseoliveira.desafiobackcb.adapter.mapper;
+
+import github.com.lucaseoliveira.desafiobackcb.adapter.out.persistence.entity.BranchEntity;
+import github.com.lucaseoliveira.desafiobackcb.adapter.out.persistence.entity.SellerEntity;
+import github.com.lucaseoliveira.desafiobackcb.application.core.domain.Branch;
+import github.com.lucaseoliveira.desafiobackcb.application.core.domain.Seller;
+
+public class SellerMapper {
+    static public Seller toDomain(SellerEntity sellerEntity)
+    {
+        BranchEntity branchEntity = sellerEntity.getBranch();
+        return new Seller(sellerEntity.getId(),sellerEntity.getRegistration(),sellerEntity.getName(), sellerEntity.getBirthDate(),
+                sellerEntity.getCpfCnpj(), sellerEntity.getEmail(), sellerEntity.getHiringType(),
+                new Branch(branchEntity.getId(), branchEntity.getName(),branchEntity.getCnpj(),branchEntity.getCity(),
+                        branchEntity.getUf(),branchEntity.getType(), branchEntity.isActive(), branchEntity.getRegistrationDate(),
+                        branchEntity.getRegistrationDate()));
+    }
+
+}
