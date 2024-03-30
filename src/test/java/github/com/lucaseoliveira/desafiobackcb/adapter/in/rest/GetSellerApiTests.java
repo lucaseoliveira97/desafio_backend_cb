85d6a074545d57a861451cf5221102fab5f446ec
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-30 10:11:50 -0300

    :test_tube: test: Refatorando testes de buscar de vendedor

--- src/test/java/github/com/lucaseoliveira/desafiobackcb/adapter/in/rest/GetSellerApiTests.java
+++ src/test/java/github/com/lucaseoliveira/desafiobackcb/adapter/in/rest/GetSellerApiTests.java
@@ -3,6 +3,7 @@ package github.com.lucaseoliveira.desafiobackcb.adapter.in.rest;
 import github.com.lucaseoliveira.desafiobackcb.adapter.in.rest.dto.GetSellerDto;
 import github.com.lucaseoliveira.desafiobackcb.application.core.domain.Branch;
 import github.com.lucaseoliveira.desafiobackcb.application.core.domain.Seller;
+import github.com.lucaseoliveira.desafiobackcb.application.core.exceptions.InvalidSellerSearchIdException;
 import github.com.lucaseoliveira.desafiobackcb.application.core.usecase.in.GetSellersUseCase;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,6 +41,7 @@ public class GetSellerApiTests {
                 .value(dto -> assertThat(dto.get(0).id()).isEqualTo(1L));
 
     }
+    @Test
     void getSellersSuccessEmpty()
     {
         List<Seller> sellers = new ArrayList<Seller>();
@@ -51,13 +53,12 @@ public class GetSellerApiTests {
                 .expectBody(GetSellerDto[].class)
                 .value(dto -> assertThat(dto.length).isEqualTo(0));
     }
-    void getSellerSuccess()
-    {
-        List<Seller> sellers = new ArrayList<Seller>();
-        sellers.add(new Seller(1L,"reg1", "name1", null,"123", "a@a.com",
+    @Test
+    void getSellerSuccess() throws InvalidSellerSearchIdException {
+        Seller seller =  (new Seller(1L,"reg1", "name1", null,"123", "a@a.com",
                 "CLT",new Branch(0L, "branch 1", "123", "campinas","sp","CLT",
                 true, null, null)));
-        when(getSellersUseCase.getSellers()).thenReturn(sellers);
+        when(getSellersUseCase.getSeller(1L)).thenReturn(seller);
         client.get().uri("/seller/1")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
@@ -66,16 +67,13 @@ public class GetSellerApiTests {
                 .value(dto -> assertThat(dto.id()).isEqualTo(1L));
 
     }
-    void getSellerNotFound()
-    {
-        List<Seller> sellers = new ArrayList<Seller>();
-        sellers.add(new Seller(1L,"reg1", "name1", null,"123", "a@a.com",
-                "CLT",new Branch(0L, "branch 1", "123", "campinas","sp","CLT",
-                true, null, null)));
-        when(getSellersUseCase.getSellers()).thenReturn(sellers);
-        client.get().uri("/seller/2")
+    @Test
+    void getSellerNotFound() throws InvalidSellerSearchIdException {
+
+        when(getSellersUseCase.getSeller(4L)).thenThrow(InvalidSellerSearchIdException.class);
+        client.get().uri("/seller/4")
                 .accept(MediaType.APPLICATION_JSON)
                 .exchange()
-                .expectStatus().isNotFound();
+                .expectStatus().isNoContent();
     }
 }
