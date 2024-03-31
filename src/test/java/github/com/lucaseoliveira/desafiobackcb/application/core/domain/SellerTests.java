8f92b240e6fbb43f8c880cc33023abc7bde30426
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-03-31 14:49:03 -0300

    :test_tube: test: Teste de validação de matricula

--- src/test/java/github/com/lucaseoliveira/desafiobackcb/application/core/domain/SellerTests.java
+++ src/test/java/github/com/lucaseoliveira/desafiobackcb/application/core/domain/SellerTests.java
@@ -10,7 +10,7 @@ public class SellerTests {
 
     @Test
     void createSuccessSeller() throws Exception {
-        Seller seller = new Seller(1L, "reg1", "name1", null,"123","a@a.com","CLT",
+        Seller seller = new Seller(1L, "98767367-OUT", "name1", null,"123","a@a.com","CLT",
                 new Branch(1L));
         boolean isValid = seller.validate();
         assertTrue(isValid);
@@ -25,31 +25,65 @@ public class SellerTests {
 
     @Test
     void createSuccessSellerWithoutName() throws Exception {
-        Seller seller = new Seller(1L, "reg1", null, null,"123","a@a.com","CLT",
+        Seller seller = new Seller(1L, "98767367-OUT", null, null,"123","a@a.com","CLT",
                 new Branch(1L));
         assertThrows(RequiredFieldException.class, seller::validate);
     }
 
     @Test
     void createSuccessSellerWithoutCpfCnpj() throws Exception {
-        Seller seller = new Seller(1L, "reg1", "name1", null,null,"a@a.com","CLT",
+        Seller seller = new Seller(1L, "98767367-OUT", "name1", null,null,"a@a.com","CLT",
                 new Branch(1L));
         assertThrows(RequiredFieldException.class, seller::validate);
     }
 
     @Test
     void createSuccessSellerWithoutEmail() throws Exception {
-        Seller seller = new Seller(1L, "reg1", "name1", null,"123",null,"CLT",
+        Seller seller = new Seller(1L, "98767367-OUT", "name1", null,"123",null,"CLT",
                 new Branch(1L));
         assertThrows(RequiredFieldException.class, seller::validate);
     }
 
     @Test
     void createSuccessSellerWithoutHiringType() throws Exception {
-        Seller seller = new Seller(1L, "reg1", "name1", null,"123","a@a.com",null,
+        Seller seller = new Seller(1L, "98767367-OUT", "name1", null,"123","a@a.com",null,
                 new Branch(1L));
         assertThrows(RequiredFieldException.class, seller::validate);
     }
-    
+    @Test
+    void createSuccessSellerRegistrationOut() throws Exception {
+        Seller seller = new Seller(1L, "98767367-OUT", "name1", null,"123","a@a.com","CLT",
+                new Branch(1L));
+        boolean isValid = seller.validate();
+        assertTrue(isValid);
+    }
+    @Test
+    void createSuccessSellerRegistrationClt() throws Exception {
+        Seller seller = new Seller(1L, "98767367-CLT", "name1", null,"123","a@a.com","CLT",
+                new Branch(1L));
+        boolean isValid = seller.validate();
+        assertTrue(isValid);
+    }
+    @Test
+    void createSuccessSellerRegistrationPj() throws Exception {
+        Seller seller = new Seller(1L, "98767367-PJ", "name1", null,"123","a@a.com","CLT",
+                new Branch(1L));
+        boolean isValid = seller.validate();
+        assertTrue(isValid);
+    }
+
+    @Test
+    void createSuccessSellerRegistrationWithLetters() throws Exception {
+        Seller seller = new Seller(1L, "9A767367-PJ", "name1", null,"123","a@a.com","CLT",
+                new Branch(1L));
+        assertThrows(InvalidFieldExpection.class, seller::validate);
+    }
+
+    @Test
+    void createSuccessSellerRegistrationWithAnotherTermination() throws Exception {
+        Seller seller = new Seller(1L, "93767367-AC", "name1", null,"123","a@a.com","CLT",
+                new Branch(1L));
+        assertThrows(InvalidFieldExpection.class, seller::validate);
+    }
 
 }
