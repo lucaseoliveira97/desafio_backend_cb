406a435be98239df018e4c2c1d6c13db29a48b39
lucaseoliveira <lucas.eoliveira97@gmail.com>
2024-04-01 19:46:12 -0300

    :bulb: docs: Mock de dados

--- build/resources/main/data.sql
+++ build/resources/main/data.sql
@@ -1,3 +1,6 @@
 INSERT INTO BRANCHES (name, cnpj, city, uf,type,active ,registration_date) VALUES
     ('Branch1', '123123123123', 'Sao Paulo', 'SP', 'type1', TRUE, {ts '2021-09-17 18:47:52.69'}),
-    ('Branch2', '456456456456', 'Rio de Janeiro', 'RJ', 'type2', TRUE, {ts '2022-01-20 10:27:32.59'});
+    ('Branch2', '456456456456', 'Rio de Janeiro', 'RJ', 'type2', TRUE, {ts '2022-01-20 10:27:32.59'}),
+    ('Branch3', '789789789789', 'Osasco', 'SP', 'type2', TRUE, {ts '2024-01-10 11:42:17.59'});
+
+
